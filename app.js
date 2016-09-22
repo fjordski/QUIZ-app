4d057259f62506821cf73841f9dfeb2f57007f23
fjordski <fordheacock@gmail.com>
2016-09-22 13:47:09 -0500

    Wooooo
    It works.

--- app.js
+++ app.js
@@ -1,31 +1,69 @@
-(function(){
+
 //variables
-var next = $('#next');
-var previous = $('#previous');
 var counter = 0;
-
-
+var question = $('.question');
+//user answer array
+var a_array = [];
+var questionCount = 0;
+var correctAnswerCounter = 0;
+//hide elements before start
+$('.question').hide();
+$('.answerBox').hide();
+$('#next').hide();
+$('#previous').hide();
 //array of questions    
 var allQuestions = [
     {
         question: "Who is the president of the USA?",
-        choices: ["Barack", "Hillary", "Satan"],
-        correctAnswer: 1
+        correctAnswer: "Barack Obama"
     },
 
     {
         question: "What is my middle name?",
-        choices: ["Walker", "Billy", "Steven"],
-        correctAnswer: 1
+        correctAnswer: "Walker"
     },
 
     {
         question: "What is my wife's name?",
-        choices: ["Steve", "Haley", "Whopper"],
-        correctAnswer: 2
+        correctAnswer: "Haley"
     },
 
 ];
+
+
+
+//on start button click, display elements
+$('.start span').click(function(){
+    $('.start span').hide();
+    $('.question').show();
+    $('.answerBox').show();
+    $('#next').show();
+    $('#previous').show();
+    question.append(allQuestions[0].question)
+});
+
+//end quiz
+function endGame() {
+if (questionCount === 3) {
+    $('.question').hide();
+    $('.answerBox').hide();
+    $('#next').hide();
+    $('#previous').hide();
+    $('#title').hide();
+    $('#finalscore').text("You got " + scoreBoard() + " answers right.");
+} else {
+    console.log("He's still playing...");
+}
+}
+
+//on submit, check answer, proceed to next question button, clear input 
+$('#submit').click(function(){
+    a_array.push($('#answer').val());
+    displayNext();
+    questionCount++;
+    endGame();
+    console.log(questionCount);
+});
 
 //function to load next question
 function displayNext () {
@@ -33,48 +71,19 @@ function displayNext () {
         counter = counter + 1;
         counter = counter % allQuestions.length;
         question.empty();
+        $("#answer").val("");
         question.append(allQuestions[counter].question);
-
 }
 
-//function to load previous question
-function displayPrevious () {
-        var question = $('.question');
-         if (counter === 0) { // i would become 0
-        counter = allQuestions.length; // so put it at the other end of the array
+//display final score
+function scoreBoard() {
+    if (a_array[0] == allQuestions[0].correctAnswer){
+        correctAnswerCounter++;
+    } if (a_array[1] == allQuestions[1].correctAnswer) {
+        correctAnswerCounter++;
+    } if (a_array[2] == allQuestions[2].correctAnswer) {
+        correctAnswerCounter++;
     }
-        counter = counter - 1; // decrease by one
-        question.empty();
-        question.append(allQuestions[counter].question);
+    return correctAnswerCounter;
 }
 
-//answers
-var opt1 = $('#opt1');
-var opt2 = $('#opt2');
-var opt3 = $('#opt3');
-
-//load answers for loaded question
-function loadAnswers (){
-        opt1.text("butt");
-}
-
-
-//next button functionality
-$('#next').click(function(){
-    displayNext();
-    loadAnswers();
-});
-
-//previous button functionality 
-$('#previous').click(function(){
-    displayPrevious();
-});
-
-
-
-
-
-
-
-
-})();
